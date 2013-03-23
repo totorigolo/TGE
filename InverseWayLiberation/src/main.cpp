a8a4a8d892dd54fe652d10a0b78f89dc2df9ac03
Thomas Lacroix <toto.rigolo@free.fr>
2013-03-24 00:53:46 +0100

    Lua + LuaBind ajoutés -> court exemple dans le main :D

--- InverseWayLiberation/src/main.cpp
+++ InverseWayLiberation/src/main.cpp
@@ -1,7 +1,31 @@
 #include "App/App.h"
+#include <lua.hpp>
+#include <luabind/luabind.hpp>
+#include <iostream>
 
 int main()
 {
+	// Crée un Lua State
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+
+	// Connecte LuaBind à ce Lua State
+	luabind::open(L);
+
+	// Define a lua function that we can call
+	luaL_dostring(
+		L,
+		"function hello()\n"
+		"  print [[Hello from Lua !]]\n"
+		"end\n"
+		);
+	
+	luabind::call_function<void>(L, "hello");
+
+	lua_close(L);
+
+	/////////////////////////////////////////////////////////////////////////
+
     App app;
     return app.Execute();
 }
