1d4a5a1e4f1a3ee8861f21bdbea1c854c25b4ac5
Thomas Lacroix <toto.rigolo@free.fr>
2013-03-27 14:54:24 +0100

    Catch des exceptions dans le main en Release

--- InverseWayLiberation/src/main.cpp
+++ InverseWayLiberation/src/main.cpp
@@ -1,7 +1,28 @@
 #include "App/App.h"
+#include <iostream>
 
 int main()
 {
-    App app;
-    return app.Execute();
+	// Valeur de retour
+	int r = 0;
+
+	// Pas de gestion des exceptions globales en debug
+#ifndef _DEBUG
+	try {
+#endif
+
+		// Crée et lance l'App
+		App app;
+		r = app.Execute();
+
+#ifndef _DEBUG
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+#endif
+
+	// Termine l'application
+    return r;
 }
