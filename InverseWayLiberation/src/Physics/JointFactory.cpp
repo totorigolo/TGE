28c6ed1e5922c7652858db3f46821a2192a4d399
Thomas Lacroix <toto.rigolo@free.fr>
2013-04-17 22:55:13 +0200

    JointFactory complétée

--- InverseWayLiberation/src/Physics/JointFactory.cpp
+++ InverseWayLiberation/src/Physics/JointFactory.cpp
@@ -1,20 +1,94 @@
 #include "JointFactory.h"
-#include "PhysicManager.h"
-#include "Joint.h"
 #include "Joints\DistanceJoint.h"
+#include "Joints\FrictionJoint.h"
+#include "Joints\GearJoint.h"
+#include "Joints\PrismaticJoint.h"
+#include "Joints\PulleyJoint.h"
+#include "Joints\RevoluteJoint.h"
+#include "Joints\RopeJoint.h"
+#include "Joints\WeldJoint.h"
+#include "Joints\WheelJoint.h"
 
 namespace JointFactory
 {
-	// Variables privés
-	namespace
+	/* Crée un DistanceJoint */
+	// Retourne l'ID du joint
+	int CreateDistanceJoint(b2Body *b1, b2Vec2 pt1, b2Body *b2, b2Vec2 pt2, float frequencyHz, float damping, bool collideconnected)
 	{
-		//...
+		auto j = new DistanceJoint(b1, pt1, b2, pt2, frequencyHz, damping, collideconnected);
+		return j->GetID();
 	}
 	
-	// Crée un DistanceJoint
-	void CreateDistanceJoint(b2Body *b1, b2Vec2 pt1, b2Body *b2, b2Vec2 pt2, float frequencyHz, float damping, bool collideconnected)
+	/* Crée un FrictionJoint */
+	// Retourne l'ID du joint
+	int CreateFrictionJoint(b2Body *b1, b2Vec2 pt1, b2Body *b2, b2Vec2 pt2, float maxForce, float maxTorque, bool collideconnected)
 	{
-		new DistanceJoint(b1, pt1, b2, pt2, frequencyHz, damping, collideconnected);
+		auto j = new FrictionJoint(b1, pt1, b2, pt2, maxForce, maxTorque, collideconnected);
+		return j->GetID();
+	}
+
+	/* Crée un GearJoint */
+	// Retourne l'ID du joint
+	int CreateGearJoint(b2Body *b1, b2Body *b2, int j1, int j2, float ratio, bool collideconnected)
+	{
+		auto j = new GearJoint(b1, b2, j1, j2, ratio, collideconnected);
+		return j->GetID();
+	}
+
+	/* Crée un PrismaticJoint */
+	// Retourne l'ID du joint
+	int CreatePrismaticJoint(b2Body *b1, b2Body *b2, b2Vec2 anchor, b2Vec2 axis, bool enableLimit, float lowerTranslation, float upperTranslation,
+																				 bool enableMotor, float motorSpeed, float maxMotorForce,
+																				 bool collideconnected)
+	{
+		auto j = new PrismaticJoint(b1, b2, anchor, axis, enableLimit, lowerTranslation, upperTranslation,
+														  enableMotor, motorSpeed, maxMotorForce,
+														  collideconnected);
+		return j->GetID();
+	}
+
+	/* Crée un PulleyJoint */
+	// Retourne l'ID du joint
+	int CreatePulleyJoint(b2Body *b1, b2Vec2 pt1, b2Body *b2, b2Vec2 pt2, b2Vec2 groundPt1, b2Vec2 groundPt2, float ratio, bool collideconnected)
+	{
+		auto j = new PulleyJoint(b1, pt1, b2, pt2, groundPt1, groundPt2, ratio, collideconnected);
+		return j->GetID();
+	}
+
+	/* Crée un RevoluteJoint */
+	// Retourne l'ID du joint
+	int CreateRevoluteJoint(b2Body *b1, b2Body *b2, b2Vec2 anchor, bool enableLimit, float lowerAngle, float upperAngle,
+																   bool enableMotor, float motorSpeed, float maxMotorTorque,
+																   bool collideconnected)
+	{
+		auto j = new RevoluteJoint(b1, b2, anchor, enableLimit, lowerAngle, upperAngle, enableMotor, motorSpeed, maxMotorTorque, collideconnected);
+		return j->GetID();
+	}
+
+	/* Crée un RopeJoint */
+	// Retourne l'ID du joint
+	int CreateRopeJoint(b2Body *b1, b2Vec2 pt1, b2Body *b2, b2Vec2 pt2, float maxLength, bool collideconnected)
+	{
+		auto j = new RopeJoint(b1, pt1, b2, pt2, maxLength, collideconnected);
+		return j->GetID();
+	}
+
+	/* Crée un WeldJoint */
+	// Retourne l'ID du joint
+	int CreateWeldJoint(b2Body *b1, b2Body *b2, b2Vec2 anchor, float frequencyHz, float damping, bool collideconnected)
+	{
+		auto j = new WeldJoint(b1, b2, anchor, frequencyHz, damping, collideconnected);
+		return j->GetID();
+	}
+
+	/* Crée un WheelJoint */
+	// Retourne l'ID du joint
+	int CreateWheelJoint(b2Body *car, b2Body *wheel, b2Vec2 pWheel, b2Vec2 axis, float frequencyHz, float damping,
+																				 bool enableMotor, float motorSpeed, float maxMotorTorque,
+																				 bool collideconnected)
+	{
+		auto j = new WheelJoint(car, wheel, pWheel, axis, frequencyHz, damping, enableMotor, motorSpeed, maxMotorTorque, collideconnected);
+		return j->GetID();
 	}
 
 } // namespace JointFactory
--- InverseWayLiberation/src/Physics/JointFactory.h
+++ InverseWayLiberation/src/Physics/JointFactory.h
@@ -1,10 +1,49 @@
 #pragma once
 #include <Box2D\Box2D.h>
 
-class Joint;
 namespace JointFactory
 {
-	// Crée un DistanceJoint
-	void CreateDistanceJoint(b2Body *b1, b2Vec2 pt1, b2Body *b2, b2Vec2 pt2, float frequencyHz = 4.f, float damping = 0.5f, bool collideconnected = true);
+	/* Crée un DistanceJoint */
+	// Retourne l'ID du joint
+	int CreateDistanceJoint(b2Body *b1, b2Vec2 pt1, b2Body *b2, b2Vec2 pt2, float frequencyHz = 4.f, float damping = 0.5f, bool collideconnected = true);
+	
+	/* Crée un FrictionJoint */
+	// Retourne l'ID du joint
+	int CreateFrictionJoint(b2Body *b1, b2Vec2 pt1, b2Body *b2, b2Vec2 p2, float maxForce = 0.f, float maxTorque = 0.f, bool collideconnected = true);
+
+	/* Crée un GearJoint */
+	// Retourne l'ID du joint
+	int CreateGearJoint(b2Body *b1, b2Body *b2, int j1, int j2 , float ratio = 1.f, bool collideconnected = true);
+
+	/* Crée un PrismaticJoint */
+	// Retourne l'ID du joint
+	int CreatePrismaticJoint(b2Body *b1, b2Body *b2, b2Vec2 anchor, b2Vec2 axis,
+							bool enableLimit = false, float lowerTranslation = 0.f, float upperTranslation = 0.f,
+							bool enableMotor = false, float motorSpeed = 0.f, float maxMotorForce = 10.f,
+							bool collideconnected = false);
+
+	/* Crée un PulleyJoint */
+	// Retourne l'ID du joint
+	int CreatePulleyJoint(b2Body *b1, b2Vec2 pt1, b2Body *b2, b2Vec2 p2, b2Vec2 groundP1, b2Vec2 groundP2, float ratio = 1.f, bool collideconnected = true);
+
+	/* Crée un RevoluteJoint */
+	// Retourne l'ID du joint
+	int CreateRevoluteJoint(b2Body *b1, b2Body *b2, b2Vec2 anchor, bool enableLimit = false, float lowerAngle = 0.f, float upperAngle = 0.f,
+																   bool enableMotor = false, float motorSpeed = 0.f, float maxMotorTorque = 10.f,
+																   bool collideconnected = false);
+
+	/* Crée un RopeJoint */
+	// Retourne l'ID du joint
+	int CreateRopeJoint(b2Body *b1, b2Vec2 pt1, b2Body *b2, b2Vec2 p2, float maxLength, bool collideconnected = true);
+
+	/* Crée un WeldJoint */
+	// Retourne l'ID du joint
+	int CreateWeldJoint(b2Body *b1, b2Body *b2, b2Vec2 anchor, float frequencyHz = 4.f, float damping = 0.5f, bool collideconnected = false);
+
+	/* Crée un WheelJoint */
+	// Retourne l'ID du joint
+	int CreateWheelJoint(b2Body *car, b2Body *wheel, b2Vec2 pWheel, b2Vec2 axis, float frequencyHz = 2.f, float damping = 0.7f,
+																				 bool enableMotor = false, float motorSpeed = 0.f, float maxMotorTorque = 10.f,
+																				 bool collideconnected = false);
 
 };
